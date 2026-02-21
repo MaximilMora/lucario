6de8d27a445145c4bcf93ac583e0456a432a73ee
MaximilMora <91358605+MaximilMora@users.noreply.github.com>
2026-02-21 10:25:59 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,8 @@ function handleOptions(req: Request) {
       status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+        'Access-Control-Allow-Methods':
+          'GET, POST, PUT, PATCH, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Max-Age': '86400',
       },
